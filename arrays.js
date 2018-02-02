1bcc316ab66db3b10cda421ed1776c581cda5892
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-02 17:07:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index) {
   return array[2];
 }
 
-function destructivelyRemoveElementToEndOfArray(array, element) {
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift(element);
   return array;
 }
